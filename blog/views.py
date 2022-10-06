e7f0974580beeb55212217668407ebc30e61328e
hbsd <hbsd@protonmail.com>
2022-10-06 20:50:22 +0330

    Add permissions and authorization

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+	LoginRequiredMixin,
+	UserPassesTestMixin
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -15,19 +19,31 @@ class BlogDetailView(DetailView):
 	template_name = 'post_detail.html'
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	template_name = 'post_new.html'
 	fields = ['title', 'author', 'body']
 
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 
-class BlogUpdateView(UpdateView):
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
 	template_name = 'post_edit.html'
 	fields = ['title', 'body']
 
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author == self.request.user
 
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Post
 	template_name = 'post_delete.html'
 	success_url = reverse_lazy('home')
+
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author == self.request.user
